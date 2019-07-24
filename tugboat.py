1e43c8f3d778e29f30f69eeff5bbac63a6f93b59
マイケル・アイゼマン <japanese_kitkat@yahoo.co.jp>
2019-07-24 07:40:22 -0400

    add config

--- tugboat.py
+++ tugboat.py
@@ -4,10 +4,12 @@ import tornado.ioloop
 import tornado.web
 import logging
 import json
+import toml
+import os
 
 class MainHandler(tornado.web.RequestHandler):
     def initialize(self):
-        self.logger = logging.getLogger()
+        self.logger = logging.getLogger('tugboat')
         self.logger.setLevel(logging.INFO)
 
     def post(self):
@@ -19,7 +21,20 @@ def set_sail():
         (r"/", MainHandler),
         ])
 
+def load_config(config_name='config.toml'):
+    config = {}
+    if not os.path.isfile(config_name):
+        print('Missing config.toml!\nDid you make sure to rename config.toml.default?')
+        exit(1)
+    with open(config_name, 'r', encoding='utf-8') as fp:
+        config.update(toml.load(fp))
+    
+    return config
+
+
 if __name__ == "__main__":
-    boat = set_sail()
-    boat.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    config = load_config()
+    print(config)
+    # boat = set_sail()
+    # boat.listen(8888)
+    # tornado.ioloop.IOLoop.current().start()
