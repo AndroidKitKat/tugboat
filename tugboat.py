556a8925e35ddf42ad4cd343a0b450f6ce36511f
マイケル・アイゼマン <japanese_kitkat@yahoo.co.jp>
2019-07-24 14:13:17 -0400

    it works!

--- tugboat.py
+++ tugboat.py
@@ -7,6 +7,13 @@ import logging
 import json
 import toml
 import os
+import pprint
+import subprocess
+
+#globals/defaults
+
+repo_names = []
+repo_paths = []
 
 class MainHandler(tornado.web.RequestHandler):
     def initialize(self):
@@ -14,12 +21,25 @@ class MainHandler(tornado.web.RequestHandler):
         self.logger.setLevel(logging.INFO)
 
     def post(self):
-        self.logger.info(self.request.arguments)
+        self.logger.info('recieved event from GitHub')
+        data = json.loads(self.request.body.decode('utf-8'))
+        get_the_right_stuff(data)
 
 def set_sail():
     return tornado.web.Application([
         (r"/", MainHandler),
         ])
+
+def get_the_right_stuff(data):
+    repo_name = data['repository']['name']
+    if repo_name in repo_names:
+        repo_path = repo_paths[repo_names.index(repo_name)]
+        pull_the_lever(repo_path)
+
+def pull_the_lever(repo_path):
+    print('pulling {}'.format(repo_path))
+    command = subprocess.run(["git", "-C", repo_path, "pull"], check=True)
+
 
 def load_config(config_name='config.toml'):
     config = {}
@@ -33,9 +53,25 @@ def load_config(config_name='config.toml'):
 
 
 if __name__ == "__main__":
+    #dirty configuration
     config = load_config()
     port = config['connection']['port']
     address = config['connection']['address']
+    repo_names = config['repos']['repo_names']
+    repo_paths = config['repos']['repo_paths']
+    if len(repo_names) != len(repo_paths):
+        print('You must have an equal number of repo names and repo paths\nPlease check your configuration file')
+        exit(1)
+    
+    #check that everything is a repo
+    for path in repo_paths:
+        name = repo_names[repo_paths.index(path)]
+        if os.path.isdir(path + '/.git'):
+            print('{} is a git repo located at {}'.format(name,path ))
+        else:
+            print('There is no git repo named {} at {}, exiting...'.format(name, path))
+            exit(1)
+
 
     boat = set_sail()
     try:
